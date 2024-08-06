cdb5a2f4fa37353f0ac5e413a6301c86431ff1d4
FreelancerMamun <mamunkhan16345@gmail.com>
2024-08-06 14:12:32 +0600

    add navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: {
+          50: '#fcf6f0',
+          100: '#f7eadd',
+          200: '#eed2ba',
+          300: '#e3b28e',
+          400: '#d78d60',
+          500: '#ce7041',
+          600: '#bf5a36',
+          700: '#9f462f',
+          800: '#803a2c',
+          900: '#683226',
+          950: '#381712',
+        },
       },
     },
   },
